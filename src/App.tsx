c296bf1f125bc5b66d220be8395eebcd973131b8
Saw Zwe Wai Yan <saw@sales-mind.ai>
2024-11-06 18:10:53 +0700

    Feat: FAQ Section Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 const App = () => {
   return (
     <main className="overflow-hidden">
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
     </main>
   );
 };
